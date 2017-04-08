857944512f06072b18e90b5540c9d3ee9aeacfbc
Jack Andersen <jackoalan@gmail.com>
2017-04-08 10:39:55 -1000

    Add normalize option to CUnitVector3f constructor

--- include/zeus/CUnitVector.hpp
+++ include/zeus/CUnitVector.hpp
@@ -11,7 +11,11 @@ public:
     ZE_DECLARE_ALIGNED_ALLOCATOR();
 
     CUnitVector3f() : CVector3f(0, 1, 0) {}
-    CUnitVector3f(float x, float y, float z) : CVector3f(x, y, z) {}
+    CUnitVector3f(float x, float y, float z, bool doNormalize = true) : CVector3f(x, y, z)
+    {
+        if (doNormalize && canBeNormalized())
+            normalize();
+    }
     CUnitVector3f(const CVector3f& vec, bool doNormalize = true) : CVector3f(vec)
     {
         if (doNormalize && canBeNormalized())

5db81ca788b4ec50b41ab0c252dd3a32f7ba9dd1
Jack Andersen <jackoalan@gmail.com>
2017-04-13 09:27:59 -1000

    Add CColor::rgbDot()

--- include/zeus/CColor.hpp
+++ include/zeus/CColor.hpp
@@ -290,6 +290,24 @@ public:
 #endif
     }
 
+    inline float rgbDot(const CColor& rhs) const
+    {
+#if __SSE__
+        TVectorUnion result;
+#if __SSE4_1__ || __SSE4_2__
+        if (cpuFeatures().SSE41 || cpuFeatures().SSE42)
+        {
+            result.mVec128 = _mm_dp_ps(mVec128, rhs.mVec128, 0x71);
+            return result.v[0];
+        }
+#endif
+        result.mVec128 = _mm_mul_ps(mVec128, rhs.mVec128);
+        return result.v[0] + result.v[1] + result.v[2];
+#else
+        return (r * rhs.r) + (g * rhs.g) + (b * rhs.b);
+#endif
+    }
+
     union {
         struct
         {

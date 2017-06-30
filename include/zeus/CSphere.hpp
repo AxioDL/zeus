8c1ddd86b194d046a41978eed93812841587618c
Jack Andersen <jackoalan@gmail.com>
2017-06-29 15:38:57 -1000

    Make CSphere::getSurfaceNormal const

--- include/zeus/CSphere.hpp
+++ include/zeus/CSphere.hpp
@@ -12,7 +12,7 @@ public:
 
     CSphere(const CVector3f& position, float radius) : position(position), radius(radius) {}
 
-    inline CVector3f getSurfaceNormal(const CVector3f& coord) { return (position - coord).normalized(); }
+    inline CVector3f getSurfaceNormal(const CVector3f& coord) const { return (position - coord).normalized(); }
 
     inline bool intersects(const CSphere& other)
     {

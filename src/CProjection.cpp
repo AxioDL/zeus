5b3ab7ac22326c7fc47c9720d55de180a87b6f4b
Jack Andersen <jackoalan@gmail.com>
2017-11-27 16:46:40 -1000

    Fix frustum culling

--- src/CProjection.cpp
+++ src/CProjection.cpp
@@ -26,7 +26,7 @@ void CProjection::_updateCachedMatrix()
         tmp = 1.0f / (m_ortho.zfar - m_ortho.znear);
         m_mtx.m[0][2] = 0.0f;
         m_mtx.m[1][2] = 0.0f;
-        m_mtx.m[2][2] = -(1.0f) * tmp;
+        m_mtx.m[2][2] = -tmp;
         m_mtx.m[3][2] = -m_ortho.zfar * tmp;
 
         m_mtx.m[0][3] = 0.0f;
@@ -36,26 +36,33 @@ void CProjection::_updateCachedMatrix()
     }
     else if (m_projType == EProjType::Perspective)
     {
-        float cot, tmp;
-        float t_fovy = std::tan(m_persp.fov / 2.0f);
+        float tfov = std::tan(m_persp.fov * 0.5f);
+        float top = m_persp.znear * tfov;
+        float bottom = -top;
+        float right = m_persp.aspect * m_persp.znear * tfov;
+        float left = -right;
 
-        cot = 1.0f / t_fovy;
+        float rml = right - left;
+        float rpl = right + left;
+        float tmb = top - bottom;
+        float tpb = top + bottom;
+        float fpn = m_persp.zfar + m_persp.znear;
+        float fmn = m_persp.zfar - m_persp.znear;
 
-        m_mtx.m[0][0] = cot / m_persp.aspect;
+        m_mtx.m[0][0] = 2.f * m_persp.znear / rml;
         m_mtx.m[1][0] = 0.0f;
-        m_mtx.m[2][0] = 0.0f;
+        m_mtx.m[2][0] = rpl / rml;
         m_mtx.m[3][0] = 0.0f;
 
         m_mtx.m[0][1] = 0.0f;
-        m_mtx.m[1][1] = cot;
-        m_mtx.m[2][1] = 0.0f;
+        m_mtx.m[1][1] = 2.f * m_persp.znear / tmb;
+        m_mtx.m[2][1] = tpb / tmb;
         m_mtx.m[3][1] = 0.0f;
 
-        tmp = 1.0f / (m_persp.zfar - m_persp.znear);
         m_mtx.m[0][2] = 0.0f;
         m_mtx.m[1][2] = 0.0f;
-        m_mtx.m[2][2] = -m_persp.zfar * tmp;
-        m_mtx.m[3][2] = -(m_persp.zfar * m_persp.znear) * tmp;
+        m_mtx.m[2][2] = -fpn / fmn;
+        m_mtx.m[3][2] = -2.f * m_persp.zfar * m_persp.znear / fmn;
 
         m_mtx.m[0][3] = 0.0f;
         m_mtx.m[1][3] = 0.0f;

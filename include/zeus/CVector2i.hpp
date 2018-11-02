0fc3e5e9cad31de0e1a2c9348c8db312876a739f
Jack Andersen <jackoalan@gmail.com>
2018-11-01 22:15:50 -1000

    CVector2f-CVector2i interop

--- include/zeus/CVector2i.hpp
+++ include/zeus/CVector2i.hpp
@@ -2,6 +2,7 @@
 
 #include "Global.hpp"
 #include "zeus/Math.hpp"
+#include "CVector2f.hpp"
 
 #if ZE_ATHENA_TYPES
 #include <athena/IStreamReader.hpp>
@@ -22,6 +23,9 @@ public:
     };
     CVector2i() = default;
     CVector2i(int xin, int yin) : x(xin), y(yin) {}
+    CVector2i(const CVector2f& vec) : x(int(vec.x)), y(int(vec.y)) {}
+
+    CVector2f toVec2f() const { return CVector2f(x, y); }
 
     inline CVector2i operator+(const CVector2i& val) const
     {
@@ -46,6 +50,10 @@ public:
     inline bool operator!=(const CVector2i& other) const
     {
         return x != other.x || y != other.y;
+    }
+    inline CVector2i operator*(int val) const
+    {
+        return CVector2i(x * val, y * val);
     }
 };
 }

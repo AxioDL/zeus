f3630be9dee64378e7bcf20b4602b64bd8c810c0
Phillip Stephens <antidote.crk@gmail.com>
2021-07-11 17:56:32 -0700

    Add CVector3f->CEulerAngles constructor to CEulerAngles

--- include/zeus/CEulerAngles.hpp
+++ include/zeus/CEulerAngles.hpp
@@ -9,8 +9,9 @@ class CTransform;
 class CEulerAngles : public CVector3f {
 public:
   constexpr CEulerAngles(float x, float y, float z) : CVector3f(x, y, z) {}
-  CEulerAngles(const CQuaternion& quat);
-  CEulerAngles(const CTransform& xf);
+  explicit CEulerAngles(const CQuaternion& quat);
+  explicit CEulerAngles(const CTransform& xf);
+  explicit CEulerAngles(const CVector3f& vec) : CVector3f(vec) {}
 };
 
 } // namespace zeus
